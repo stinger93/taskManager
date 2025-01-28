2d65a6c46cc094380bb81497f5f0518160a63eab
Yaroslav <yaroslavmetal94@gmail.com>
2025-01-28 16:41:34 +0300

    db

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,9 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "";
+    private static final String USER = "";
+    private  static final String PASSWORD = "";
+}
